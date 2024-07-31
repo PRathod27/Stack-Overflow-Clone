7e3443f48699fabd88b770474ef43eb9919ee819
PRathod27 <prem.rathod.2715@gmail.com>
2024-07-31 15:01:54 +0530

    -Done

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.get('/',(req,res) =>{
 app.use('/user', userRoutes)
 app.use('/questions', questionRoutes) 
 app.use('/answers',answerRoutes)
-const  PORT = process.env.PORT || 5000
+const  PORT = process.env.PORT
 
 
 const CONNECTION_URL =  process.env.DATA_URL
